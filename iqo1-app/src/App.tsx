17b6944d064d2631ef702cdc0e12a95bcbf71ce1
Spirit-Tiger <sprttiger@gmail.com>
2022-06-26 22:58:44 +0300

    setup 3d model

--- iqo1-app/src/App.tsx
+++ iqo1-app/src/App.tsx
@@ -1,19 +1,43 @@
-import React, { useState } from 'react';
+import React, { Suspense, useEffect, useRef, useState } from 'react';
 import './App.css';
 import { FormattedMessage, IntlProvider } from 'react-intl';
 import { LOCALES } from './i18n/constants';
 import{messages} from './i18n/eng';
 import styled from 'styled-components';
+import { Canvas, useFrame, useLoader, } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import * as THREE from 'three/src/Three'
 
+
+const RenderGltf = () => {
+  const gltf = useLoader(GLTFLoader, '/scene.gltf')
+  const mesh = useRef<THREE.Mesh>(null);
+  useFrame(() => {if (mesh.current !== null) {
+    mesh.current.rotation.y += 0.01;
+  }})
+  return (
+    <mesh ref={mesh}>
+      <Suspense fallback={null}>
+        <primitive object={gltf.scene} scale={3} />
+      </Suspense>
+    </mesh>
+  )
+}
 
 function App() {
 
    const[locale, setLocale]= useState(LOCALES.ENGLISH);
    console.log(IntlProvider);
+
+  
+
   return (
     <IntlProvider messages={messages[locale]} locale={locale} >
     <div>
-      
+      <Canvas style={{height:500}}>
+          <RenderGltf />
+      </Canvas>
       <StyledMessage>
         <FormattedMessage id="english" />
       </StyledMessage>
