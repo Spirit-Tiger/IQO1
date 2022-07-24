e371206752de54dce93cd7c5b37659685c5d6a28
Ilya Pivtorak <teolastwhisper@gmail.com>
2022-07-24 11:44:19 +0300

    tradingview chart

--- iqo1-app/src/i18n/eng.js
+++ iqo1-app/src/i18n/eng.js
@@ -83,7 +83,7 @@ export const messages = {
 
        total_balance_header: "Total Balance :",
        invest_btn: "Invest",
-       withdrow_btn: "Withdrow",
+       withdrow_btn: "Withdraw",
        dashboard_title:'Dashboard', 
        dashboard_active_currency_title:'Your active:',
        dashboard_profit_title:'Profit:',
